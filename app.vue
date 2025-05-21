421de2dd1dc24f76eca8bfd48ca7ebe2e78e9288
Amanda Santos <amandasf0200@gmail.com>
2025-05-21 20:11:59 -0300

    chore: tailwind / daisy ui / icons setup (#49)
    * chore: setup TailwindCSS
    
    * chore: setup DaisyUI
    
    * chore: setup icons

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div data-theme="synthwave">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-primary">
+      View Map <Icon name="tabler:map" />
+    </button>
   </div>
 </template>
