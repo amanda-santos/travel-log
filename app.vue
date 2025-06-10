b195bb6ee5397bd1e21f89a3aaad620e52c74040
Amanda Santos <amandasf0200@gmail.com>
2025-06-09 23:17:31 -0300

    feat: adds navbar and hero

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div data-theme="synthwave">
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-primary">
-      View Map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
