92f08c71187d7a1aeb3b6456dfefa717319610c9
Amanda Santos <amandasf0200@gmail.com>
2025-06-22 20:13:22 -0300

    feat: adds authentication with Github (#55)

---
+++ stores/auth.ts
@@ -0,0 +1,24 @@
+import { createAuthClient } from "better-auth/client";
+
+const authClient = createAuthClient();
+
+export const useAuthStore = defineStore("useAuthStore", () => {
+  const isLoading = ref(false);
+
+  async function signIn() {
+    isLoading.value = true;
+
+    await authClient.signIn.social({
+      provider: "github",
+      callbackURL: "/dashboard",
+      errorCallbackURL: "/error",
+    });
+
+    isLoading.value = false;
+  }
+
+  return {
+    isLoading,
+    signIn,
+  };
+});
