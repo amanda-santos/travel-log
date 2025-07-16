7fd3d77b04e43d0d252646a5405c66da237ad61b
Amanda Santos <amandasf0200@gmail.com>
2025-07-16 18:41:29 -0300

    feat: makes slug names friendly and unique (#61)

--- lib/types.ts
+++ lib/types.ts
@@ -4,7 +4,7 @@ declare module "h3" {
   // eslint-disable-next-line ts/consistent-type-definitions
   interface H3EventContext {
     user?: Omit<User, "id"> & {
-      id: string;
+      id: number;
     };
   }
 }
