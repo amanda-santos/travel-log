2ba025cba72438a5f1c9d231d5370b8066a5812f
Amanda Santos <amandasf0200@gmail.com>
2025-07-20 18:12:08 -0300

    feat: setup map with MapLibre

---
+++ components/app/map.client.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { CENTER_USA } from "~/lib/constants";
+
+const colorMode = useColorMode();
+const style = computed(() =>
+  colorMode.value === "dark"
+    ? "/styles/dark.json"
+    : "https://tiles.openfreemap.org/styles/positron");
+const zoom = 0;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="CENTER_USA"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
