31b3c6839d1243b45d57b44d33c4b5387fe186d2
Amanda Santos <amandasf0200@gmail.com>
2025-07-20 18:18:25 -0300

    fix: map theme toggle

--- components/app/map.client.vue
+++ components/app/map.client.vue
@@ -3,7 +3,7 @@ import { CENTER_USA } from "~/lib/constants";
 
 const colorMode = useColorMode();
 const style = computed(() =>
-  colorMode.value === "dark"
+  colorMode.value === "black"
     ? "/styles/dark.json"
     : "https://tiles.openfreemap.org/styles/positron");
 const zoom = 0;
